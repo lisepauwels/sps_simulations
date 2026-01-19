425bff5254d89ed74fc3771decdcf26606318666
Lise Pauwels <lise@lidp.be>
2026-01-19 11:51:35 +0100

    Results of intensity scans

---
+++ Studies/MomentumAcceptance/IntensityScan/submission_files/script_xboinc.py
@@ -0,0 +1,58 @@
+import numpy as np
+from pathlib import Path
+import sys
+import json
+
+import xobjects as xo
+import xtrack as xt
+import xpart as xp
+import xobjects as xo
+import xcoll as xc
+
+line = xt.Line.from_json('lines_rf_sweep/sps_errors_chroma_0.5.json')
+plane = 'DPpos'
+
+
+#Context and parameters
+context = xo.ContextCpu()
+
+num_particles  = 1_000_000
+sweep = 6000
+sweep = -abs(sweep) if plane == 'DPpos' else abs(sweep)
+num_turns = 6000
+nemitt_x = 2e-6
+nemitt_y = 2e-6
+sigma_z = 0.224
+
+
+#Installing TIDP
+tidp_ap_tot = 147
+block_mvt = 29
+
+tidp = xc.EverestCollimator(length=4.3, material=xc.materials.Carbon, jaw_L= tidp_ap_tot/2 + block_mvt, jaw_R = -tidp_ap_tot/2 + block_mvt)
+line.collimators.install(names=['tidp.11434'], elements=[tidp])
+
+line.build_tracker()
+
+#particles
+part = xp.generate_matched_gaussian_bunch(nemitt_x=nemitt_x,
+                                          nemitt_y=nemitt_y,
+                                          sigma_z=sigma_z, num_particles=num_particles, line=line)
+
+rf_sweep = xc.RFSweep(line)
+rf_sweep.prepare(sweep_per_turn=sweep/num_turns)
+rf_sweep.info()
+
+# Track during RF sweep:
+line.scattering.enable()
+line.track(particles=part, num_turns=num_turns, time=True, with_progress=5)
+line.scattering.disable()
+print(f"Done sweeping RF in {line.time_last_track:.1f}s.")
+
+# Save results
+sweep_per_turn = sweep/num_turns
+dico = {'sweep_per_turn': sweep_per_turn,
+        'at_turn' : part.at_turn.copy().tolist()}
+
+with open(f'death_turns.json', 'w') as fid:
+    json.dump(dico, fid, indent=4)
